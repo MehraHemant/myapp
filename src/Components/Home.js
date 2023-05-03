4f6140e0ed49e03d3ae5ca8251415c2fd81abf69
MehraHemant <hemantmehra9502@gmail.com>
2023-05-03 21:15:32 +0530

    clear

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home(props) {
+  let color = false;
+  return color ? (
+    <ul>
+      {props.fruits.map((el, i) => (
+        <li className={props.color[i]}>{el}</li>
+      ))}
+    </ul>
+  ) : (
+    <ul>
+      {props.fruits.map((el, i) => (
+        <li>{el}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default Home;
